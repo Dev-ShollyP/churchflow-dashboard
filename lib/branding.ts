e5aefec37cbf14ff9359f179c7a18dba031514eb
Dev-ShollyP <olusholashotayo05@gmail.com>
2026-09-07 08:43:09 +0100

    Add files via upload

---
+++ lib/branding.ts
@@ -0,0 +1,4 @@
+export const CHURCH_LOGO_URL = 'https://xzyrftzhaolovlbnpbpk.supabase.co/storage/v1/object/public/Flyers/logo.png';
+
+export const CHURCH_NAME = 'RCCG Everflourishing Mega Sanctuary';
+export const CHURCH_SHORT_NAME = 'RCCG EVF';
